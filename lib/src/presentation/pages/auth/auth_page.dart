a4b851d7a97b6e07b2bfd3bb45a2f5cebdb40832
bahrom2101 <jabborov0121@gmail.com>
2023-04-05 21:47:19 +0500

    some changes

--- lib/src/presentation/pages/auth/auth_page.dart
+++ lib/src/presentation/pages/auth/auth_page.dart
@@ -10,6 +10,6 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold();
   }
 }
